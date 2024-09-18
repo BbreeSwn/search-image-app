a0990629b51d89d7359421e8bbbf1b93ec06c3a7
BbreeSwn <suwanan.cs@hotmail.com>
2024-09-18 17:03:45 +0700

    feat: fetch api search image

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,12 @@
 import "./Form.css";
 import { useState } from "react";
+import Picture from "./Picture";
 
 export default function Form() {
   const [word, setWord] = useState("");
-  const key = "k8VfEWBgx_nuy1vtvHSvaJEUiNx_ucG8Q4QoOqSfy8U";
+  const [photos, setPhotos] = useState([]);
+  const { VITE_API_URL, VITE_API_KEY } = import.meta.env;
+
 
   function searchImage(e) {
     e.preventDefault();
@@ -16,24 +19,39 @@ export default function Form() {
   }
   async function fetchImageFromAPI() {
     try {
-      const url = `https://api.unsplash.com/search/photos?page=1&query=${word}&client_id=${key}`;
+      const url = `${VITE_API_URL}?per_page=15&query=${word}&client_id=${VITE_API_KEY}`;
       const res = await fetch(url);
       const data = await res.json();
-      console.log(data.result);
+      const result = data.results;
+      if (result.length == 0) {
+        alert("Image not found");
+        setWord("");
+      } else {
+        // show image
+        setPhotos(result);
+      }
     } catch (error) {
       console.log(error);
     }
   }
 
   return (
-    <form onSubmit={searchImage}>
-      <input
-        type="text"
-        placeholder="Search image"
-        value={word}
-        onChange={(e) => setWord(e.target.value)}
-      />
-      <button type="onSubmit">Search</button>
-    </form>
+    <>
+      <form onSubmit={searchImage}>
+        <input
+          type="text"
+          placeholder="Search image"
+          value={word}
+          onChange={(e) => setWord(e.target.value)}
+        />
+        <button type="onSubmit">Search</button>
+      </form>
+      {/* // show image */}
+      <div className="search-result">
+        {photos.map((data, index) => {
+          return <Picture {...data} key={index} />;
+        })}
+      </div>
+    </>
   );
 }
---
+++ src/components/Picture.jsx
@@ -0,0 +1,11 @@
+
+
+const Picture = (props) => {
+  return (
+    <>
+    <img src={props.urls.small} alt={props.description} />
+    </>
+  )
+}
+
+export default Picture
