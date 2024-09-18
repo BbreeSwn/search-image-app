eb10e142bed273024a7431017eb6bb01db8d2d56
BbreeSwn <suwanan.cs@hotmail.com>
2024-09-18 13:30:07 +0700

    feat: set state for search image

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,27 @@
-import "./Form.css"
+import "./Form.css";
+import { useState } from "react";
 
 export default function Form() {
+  const [word, setWord] = useState("");
+
+  function searchImage(e){
+    e.preventDefault()
+    if(!word){
+alert("Please enter your  keyword")
+    }else{
+      //เรียกใช้ API
+      console.log(word);
+    }
+  }
   return (
-    <form >
-      <input type="text" placeholder="Search image" />
-      <button type="onSubmit">Search</button>
+    <form onSubmit={searchImage}>
+      <input
+        type="text"
+        placeholder="Search image"
+        value={word}
+        onChange={(e) => setWord(e.target.value)}
+      />
+      <button type="onSubmit" >Search</button>
     </form>
   );
 }
