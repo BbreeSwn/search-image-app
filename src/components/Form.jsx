028ed6e01c32f77e3a302a6f4d59fcf25b7ae3d0
BbreeSwn <suwanan.cs@hotmail.com>
2024-09-18 13:09:26 +0700

    set form for search image

---
+++ src/components/Form.jsx
@@ -0,0 +1,10 @@
+import "./Form.css"
+
+export default function Form() {
+  return (
+    <form >
+      <input type="text" placeholder="Search image" />
+      <button type="onSubmit">Search</button>
+    </form>
+  );
+}
